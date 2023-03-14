eaa12455b446768f972f2636db1ecc8e72e34a38
Nikita <zeninnikita96@gmail.com>
2023-03-14 20:35:58 +0100

    finish nested dynamic routes

--- shop-frontend/pages/[category]/[item]/index.js
+++ shop-frontend/pages/[category]/[item]/index.js
@@ -1,35 +1,47 @@
 function Item({ items }) {
-  return <div>Item 123</div>;
+  const array = items.data;
+  console.log(array);
+  return (
+    <div>
+      <div>Title: {array[0].attributes.items.data[0].attributes.Title}</div>
+      <div>
+        Description: {array[0].attributes.items.data[0].attributes.Description}
+      </div>
+      <div>Price: {array[0].attributes.items.data[0].attributes.Price}USD</div>
+    </div>
+  );
 }
 
 export default Item;
 
 export const getStaticProps = async ({ params }) => {
   const { category, item } = params;
-
   const result = await fetch(
     `http://localhost:1337/api/categories?filters[name][$eq]=${category}&populate[items][filters][id][$eq]=${item}`
-    // if params.category return category name what does item return? how to check it?
   );
   const data = await result.json();
-  // console.log("data from getProps [item]", data);
-
   return {
     props: { items: data },
   };
 };
 
 export const getStaticPaths = async () => {
-  const result = await fetch(`http://localhost:1337/api/categories?populate=*`); // Error: A required parameter (category) was not provided as a string in getStaticPaths for /[category]/[item]
-  // something is wrong in this link, I don't understand where I should take this category from? I should have multiple params somehow?
+  const result = await fetch(`http://localhost:1337/api/categories?populate=*`);
+
   const data = await result.json();
   console.log("data in getStaticPaths [irwm]", data);
 
-  //change data array here to one-level structure - flatMap?
+  const flattenedData = data.data.flatMap((obj) =>
+    obj.attributes.items.data.map((item) => ({
+      id: item.id,
+      category: obj.attributes.name,
+      attributes: item.attributes,
+    }))
+  );
 
-  const paths = data.data.map((i) => {
+  const paths = flattenedData.map((i) => {
     return {
-      params: { category: i.category, item: i.id.toString() }, // should I somehow put a category name here? category name is on level up then the items, we are mapping items on their level so I would need to use two maps?
+      params: { category: i.category, item: i.id.toString() },
     };
   });
   return {
