9c74b4e5c7400765f008b166dbac82b53fa78056
Nikita <zeninnikita96@gmail.com>
2023-04-14 19:22:45 +0200

    css changes

--- shop-frontend/components/Navbar.js
+++ shop-frontend/components/Navbar.js
@@ -9,27 +9,32 @@ import Link from "next/link";
 function Navbar() {
   return (
     <div className={styles.navbar}>
-      <Image src={logo} alt="Slik Touch Logo" width={400} height={100} />
-      <div class={styles.shopdropdown}>
-        <button class={styles.shopdropdown__button}>
-          Shop
-          <FontAwesomeIcon icon={faChevronDown} className={styles.arrowdown} />
-        </button>
-        <div class={styles.shopdropdown__content}>
-          <Link href="/about" className={styles.dropdownlink}>
-            Covers
-          </Link>
-          <Link href="/about" className={styles.dropdownlink}>
-            Pillows
-          </Link>
+      <Image src={logo} alt="Slik Touch Logo" width={180} height={100} />
+      <div class={styles.navbaritems}>
+        <div class={styles.shopdropdown}>
+          <button class={styles.shopdropdown__button}>
+            Shop
+            <FontAwesomeIcon
+              icon={faChevronDown}
+              className={styles.arrowdown}
+            />
+          </button>
+          <div class={styles.shopdropdown__content}>
+            <Link href="/about" className={styles.dropdownlink}>
+              Covers
+            </Link>
+            <Link href="/about" className={styles.dropdownlink}>
+              Pillows
+            </Link>
+          </div>
         </div>
+        <Link href="/about" className={styles.navbarlink}>
+          About us
+        </Link>
+        <Link href="/contacts" className={styles.navbarlink}>
+          Contacts
+        </Link>
       </div>
-      <Link href="/about" className={styles.navbarlink}>
-        About us
-      </Link>
-      <Link href="/contacts" className={styles.navbarlink}>
-        Contacts
-      </Link>
     </div>
   );
 }
