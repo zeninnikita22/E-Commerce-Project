835493cb5b06cefb17321cf400db318bc31dce28
Nikita <zeninnikita96@gmail.com>
2023-04-14 19:29:25 +0200

    installed and deployed tanstack query

--- shop-frontend/pages/_app.js
+++ shop-frontend/pages/_app.js
@@ -1,15 +1,20 @@
 import "@/styles/globals.css";
 import Layout from "../components/Layout";
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function App({ Component, pageProps }) {
+  const queryClient = new QueryClient();
+
   return (
     <>
-      <ChakraProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ChakraProvider>
+      <QueryClientProvider>
+        <ChakraProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ChakraProvider>
+      </QueryClientProvider>
     </>
   );
 }
