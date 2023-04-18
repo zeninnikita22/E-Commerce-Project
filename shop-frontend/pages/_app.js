20f34e524694676bbddedf3fd3e803c8fdff63d2
Nikita <zeninnikita96@gmail.com>
2023-04-18 10:10:16 +0200

    created delete category mutation

--- shop-frontend/pages/_app.js
+++ shop-frontend/pages/_app.js
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import Layout from "../components/Layout";
-import { ChakraProvider } from "@chakra-ui/react";
+// import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function App({ Component, pageProps }) {
@@ -9,11 +9,11 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <ChakraProvider>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </ChakraProvider>
+        {/* <ChakraProvider> */}
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+        {/* </ChakraProvider> */}
       </QueryClientProvider>
     </>
   );
--- shop-frontend/pages/about.js
+++ shop-frontend/pages/about.js
@@ -1,7 +1,12 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { resolve } from "styled-jsx/css";
+import { useState } from "react";
 
 function About() {
+  const [categoryName, setCategoryName] = useState("");
+  // const [deleteCategoryId, setDeleteCategoryId] = useState();
+  const queryClient = useQueryClient();
+
   const axios = require("axios");
   const categoryQuery = useQuery({
     queryKey: ["categories"],
@@ -13,30 +18,108 @@ function About() {
         // console.log(data);
       ),
   });
+  const newCategoryMutation = useMutation({
+    mutationFn: (newCategory) =>
+      axios
+        .post(
+          `http://localhost:1337/api/categories/`,
+          newCategory
+          // newCategory
+        )
+        .then((response) => response.data),
+  });
+  const deleteCategoryMutation = useMutation({
+    mutationFn: (deleteCategoryId) =>
+      axios
+        .delete(
+          `http://localhost:1337/api/categories/${deleteCategoryId}`
+          // newCategory
+        )
+        .then((response) => response.data),
+  });
 
   if (categoryQuery.isLoading) return <h2>Loading!</h2>;
   if (categoryQuery.isError) return <h2>Error!</h2>;
 
   console.log(categoryQuery);
-
   const dataOfCategoryQuery = categoryQuery.data;
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newCategory = {
+      data: {
+        name: categoryName,
+      },
+    };
+    newCategoryMutation.mutate(newCategory, {
+      onSuccess: async () => {
+        console.log(123);
+        await queryClient.refetchQueries("categories");
+      },
+    });
+  }
+
+  function deleteCategory({ id }) {
+    const deleteCategoryId = id;
+    // console.log(id);
+    // setDeleteCategoryId(id);
+    // console.log(categoryId);
+    deleteCategoryMutation.mutate(deleteCategoryId, {
+      onSuccess: async () => {
+        console.log(456);
+        await queryClient.refetchQueries("categories");
+      },
+    });
+  }
+
+  // const handleCreatePost = (event) => {
+  //   event.preventDefault();
+  //   const newPost = {
+  //     title: event.target.elements.title.value,
+  //     body: event.target.elements.body.value,
+  //   };
+  //   createPostMutation
+  //     .mutate(newPost)
+  //     .then(() => {
+  //       queryClient.invalidateQueries("posts");
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // };
 
   return (
     <>
       <div>Temporary page for requests</div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="type a name of new category here"
+          value={categoryName}
+          onChange={(e) => setCategoryName(e.target.value)}
+        ></input>
+        <button type="submit">Add new category</button>
+      </form>
+
       <div>
         {categoryQuery.isSuccess
           ? dataOfCategoryQuery.data.map((e) => {
-              return <div>{e.attributes.name}</div>;
+              return (
+                <div key={e.id}>
+                  <div>{e.attributes.name}</div>
+                  <div>
+                    <button onClick={() => deleteCategory(e)}>Delete</button>
+                  </div>
+                </div>
+              );
             })
           : null}
       </div>
     </>
   );
-
-  // function delay(miliseconds) {
-  //   return new Promise((resolve) => setTimeout(resolve, miliseconds));
-  // }
 }
+// function delay(miliseconds) {
+//   return new Promise((resolve) => setTimeout(resolve, miliseconds));
+// }
 
 export default About;
