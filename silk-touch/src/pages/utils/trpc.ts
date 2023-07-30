e337961c11d0bdf4554b138c6aa09f929526bc31
Nikita <zeninnikita96@gmail.com>
2023-07-30 23:26:09 +0200

    minor changes

--- silk-touch/src/pages/utils/trpc.ts
+++ silk-touch/src/pages/utils/trpc.ts
@@ -9,12 +9,10 @@ function getBaseUrl() {
   }
   // When rendering on the server, we return an absolute URL
 
-  // reference for vercel.com
   if (process.env.VERCEL_URL) {
     return `https://${process.env.VERCEL_URL}`;
   }
 
-  // assume localhost
   return `http://localhost:${process.env.PORT ?? 3000}`;
 }
 
