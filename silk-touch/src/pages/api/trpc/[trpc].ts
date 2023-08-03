e8726811773ad1775fb3e54617bb2912a92ccfba
Nikita <zeninnikita96@gmail.com>
2023-08-03 21:36:51 +0200

    add prisma to the project

--- silk-touch/src/pages/api/trpc/[trpc].ts
+++ silk-touch/src/pages/api/trpc/[trpc].ts
@@ -1,7 +1,3 @@
-/**
- * This is the API-handler of your app that contains all your API routes.
- * On a bigger app, you will probably want to split this file up into multiple files.
- */
 import * as trpcNext from "@trpc/server/adapters/next";
 import { publicProcedure, router } from "../../../server/trpc";
 import { z } from "zod";
