dc2aef711f384aed8a49ebfa7b9eba844f10d561
Nikita <zeninnikita96@gmail.com>
2023-09-10 21:56:38 +0200

    working on adding next auth authentication

--- silk-touch/src/pages/[category]/index.tsx
+++ silk-touch/src/pages/[category]/index.tsx
@@ -3,11 +3,7 @@ import { trpc } from "../utils/trpc";
 import prisma from "../../../lib/prisma";
 
 export const getStaticPaths = async () => {
-  const items = await prisma.item.findMany({
-    select: {
-      category: true, /// check if thats only field we need
-    },
-  });
+  const items = await prisma.item.findMany();
 
   console.log(items);
 
@@ -17,16 +13,36 @@ export const getStaticPaths = async () => {
         category: item.category,
       },
     })),
-    // https://nextjs.org/docs/pages/api-reference/functions/get-static-paths#fallback-blocking
     fallback: "blocking",
   };
 };
 
-export default function Category() {
-  const itemsQuery = trpc.getAllItems.useQuery();
+export async function getStaticProps({ params }) {
+  const categoryName = params.category;
+  const item = await prisma.item.findMany({
+    where: { category: categoryName },
+  });
+
+  return {
+    props: {
+      item,
+    },
+  };
+}
+
+export default function Category({ item }) {
+  // const itemsQuery = trpc.getAllItems.useQuery();
+  console.log(item);
+
   return (
     <div>
-      <div>This is index page of category X</div>
+      <p>Temp</p>
+      <div>This is index page of category {item[0].category}</div>
+      <div>
+        {item.map((element) => {
+          return <div key={element.id}>{element.title}</div>;
+        })}
+      </div>
     </div>
   );
 }
--- silk-touch/src/pages/_app.tsx
+++ silk-touch/src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import "@/styles/globals.css";
 import type { AppType } from "next/app";
+// import { SessionProvider } from "next-auth/react";
 
 import { trpc } from "./utils/trpc";
 
 const App: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    // <SessionProvider session={session}>
+    <Component {...pageProps} />
+    // </SessionProvider>
+  );
 };
 
 export default trpc.withTRPC(App);
