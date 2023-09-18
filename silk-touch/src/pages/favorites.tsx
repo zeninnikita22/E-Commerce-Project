773774e9e5bad19c09939cb9c68400cb7c60d5f3
Nikita <zeninnikita96@gmail.com>
2023-09-18 23:59:16 +0200

    work on favorites

--- silk-touch/src/pages/favorites.tsx
+++ silk-touch/src/pages/favorites.tsx
@@ -4,9 +4,34 @@ import { useUser } from "@clerk/nextjs";
 
 export default function Favorites() {
   const { isLoaded, isSignedIn, user } = useUser();
+  const changeFavoritesItemsMutation = trpc.changeFavorites.useMutation();
+
+  const queryClient = useQueryClient();
+
   const favoritesQuery = trpc.getFavoritesItems.useQuery({
     userId: user?.id,
   });
+
+  function deleteFromFavorites(item) {
+    const favoritesElement = favoritesQuery.data.find(
+      (element) => element.itemId === item.id
+    );
+    console.log(favoritesQuery.data);
+    changeFavoritesItemsMutation.mutate(
+      {
+        userId: user?.id,
+        itemId: item.id,
+        favoritesId: favoritesElement === undefined ? "" : favoritesElement?.id,
+      },
+      {
+        onSuccess: (data) => {
+          // Invalidate specific queries after the mutation is successful
+          queryClient.invalidateQueries({ queryKey: ["getFavoritesItems"] });
+          console.log("Add to favorites OnSuccess", data);
+        },
+      }
+    );
+  }
 
   return (
     <div>
@@ -15,6 +40,9 @@ export default function Favorites() {
           return (
             <div key={item.id}>
               <div>{item.item.title}</div>
+              <button onClick={() => deleteFromFavorites(item)}>
+                Delete from favorites
+              </button>
             </div>
           );
         })}
