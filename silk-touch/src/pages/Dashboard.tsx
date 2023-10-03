a86d11e0bc6d2e721b9baefedd215ad340220543
Nikita <zeninnikita96@gmail.com>
2023-10-03 17:10:30 +0200

    work on UI

--- silk-touch/src/pages/Dashboard.tsx
+++ silk-touch/src/pages/Dashboard.tsx
@@ -7,8 +7,11 @@ import { useAuth } from "@clerk/nextjs";
 import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/router";
 import { loadStripe } from "@stripe/stripe-js";
+import { Fragment } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
 
-const Dashboard = () => {
+const Dashboard = ({ openDashboard, setOpenDashboard }) => {
   const stripePromise = loadStripe(
     process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
   );
@@ -134,45 +137,190 @@ const Dashboard = () => {
 
   return (
     <>
-      <p>/// This is hidden dashboard ///</p>
-      <h1>Cart Items:</h1>
-      <div>
-        {cartQuery.data?.map((element) => {
-          return (
-            <div key={element.id}>
-              <button onClick={() => addToCart(element)}>+</button>
-              <div>{element.item.title}</div>
-              <input
-                type="number"
-                min="0"
-                max="99"
-                value={
-                  cartQuery.data?.filter(
-                    (cartItem) => cartItem.itemId === element.item.id
-                  )[0].quantity
-                }
-                onChange={(e) => changeItemQuantity({ e, element })}
-              ></input>
-              <div>{element.quantity}</div>
-              <div>{element.item.price * element.quantity}</div>
-              <button onClick={() => decreaseItemQuantity(element)}>-</button>
-              <button onClick={() => deleteFromCart(element)}>DEL</button>
-            </div>
-          );
-        })}
-        <div>
-          TOTAL PRICE:{" "}
-          {cartQuery.data?.reduce(
-            (acc, item) => acc + item.item.price * item.quantity,
-            0
-          )}
-        </div>
-        <button onClick={() => checkout()}>Checkout</button>
-      </div>
+      <Transition.Root show={openDashboard} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={setOpenDashboard}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-in-out duration-500"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in-out duration-500"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
 
-      <div>
-        ICON and Number of items in a cart: <CartIcon />
-      </div>
+          <div className="fixed inset-0 overflow-hidden">
+            <div className="absolute inset-0 overflow-hidden">
+              <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+                <Transition.Child
+                  as={Fragment}
+                  enter="transform transition ease-in-out duration-500 sm:duration-700"
+                  enterFrom="translate-x-full"
+                  enterTo="translate-x-0"
+                  leave="transform transition ease-in-out duration-500 sm:duration-700"
+                  leaveFrom="translate-x-0"
+                  leaveTo="translate-x-full"
+                >
+                  <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
+                    <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+                      <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
+                        <div className="flex items-start justify-between">
+                          <Dialog.Title className="text-lg font-medium text-gray-900">
+                            Shopping cart
+                          </Dialog.Title>
+                          <div className="ml-3 flex h-7 items-center">
+                            <button
+                              type="button"
+                              className="relative -m-2 p-2 text-gray-400 hover:text-gray-500"
+                              onClick={() => setOpenDashboard(false)}
+                            >
+                              <span className="absolute -inset-0.5" />
+                              <span className="sr-only">Close panel</span>
+                              <XMarkIcon
+                                className="h-6 w-6"
+                                aria-hidden="true"
+                              />
+                            </button>
+                          </div>
+                        </div>
+
+                        <div className="mt-8">
+                          <div className="flow-root">
+                            <ul
+                              role="list"
+                              className="-my-6 divide-y divide-gray-200"
+                            >
+                              {cartQuery.data?.map((element) => {
+                                return (
+                                  <li key={element.id} className="flex py-6">
+                                    <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                                      <img
+                                        src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg"
+                                        alt="Tall slender porcelain bottle with natural clay textured body and cork stopper."
+                                        className="h-full w-full object-cover object-center"
+                                      />
+                                    </div>
+                                    <div className="ml-4 flex flex-1 flex-col">
+                                      <div>
+                                        <div className="flex justify-between text-base font-medium text-gray-900">
+                                          <h3>
+                                            <a href="#">{element.item.title}</a>
+                                          </h3>
+                                          <p className="ml-4">
+                                            {element.item.price}
+                                          </p>
+                                        </div>
+                                        <p className="mt-1 text-sm text-gray-500">
+                                          {element.item.content}
+                                        </p>
+                                      </div>
+                                      <div className="flex flex-1 items-end justify-between text-sm">
+                                        <p className="text-gray-500">
+                                          Qty {element.quantity}
+                                        </p>
+
+                                        <div className="flex">
+                                          <button
+                                            type="button"
+                                            className="font-medium text-indigo-600 hover:text-indigo-500"
+                                          >
+                                            Remove
+                                          </button>
+                                        </div>
+                                      </div>
+                                    </div>
+                                    {/* <button onClick={() => addToCart(element)}>
+                                      +
+                                    </button>
+                                    <div>{element.item.title}</div>
+                                    <input
+                                      type="number"
+                                      min="0"
+                                      max="99"
+                                      value={
+                                        cartQuery.data?.filter(
+                                          (cartItem) =>
+                                            cartItem.itemId === element.item.id
+                                        )[0].quantity
+                                      }
+                                      onChange={(e) =>
+                                        changeItemQuantity({ e, element })
+                                      }
+                                    ></input>
+                                    <div>{element.quantity}</div>
+                                    <div>
+                                      {element.item.price * element.quantity}
+                                    </div>
+                                    <button
+                                      onClick={() =>
+                                        decreaseItemQuantity(element)
+                                      }
+                                    >
+                                      -
+                                    </button>
+                                    <button
+                                      onClick={() => deleteFromCart(element)}
+                                    >
+                                      DEL
+                                    </button> */}
+                                  </li>
+                                );
+                              })}
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
+                        <div className="flex justify-between text-base font-medium text-gray-900">
+                          $
+                          {cartQuery.data?.reduce(
+                            (acc, item) =>
+                              acc + item.item.price * item.quantity,
+                            0
+                          )}
+                        </div>
+                        <p className="mt-0.5 text-sm text-gray-500">
+                          Shipping and taxes calculated at checkout.
+                        </p>
+                        <div className="mt-6">
+                          {/* <a
+                            href="#"
+                            className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                          >
+                            Checkout
+                          </a> */}
+                          <button
+                            onClick={() => checkout()}
+                            className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                          >
+                            Checkout
+                          </button>
+                        </div>
+                        <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
+                          <p>
+                            or
+                            <button
+                              type="button"
+                              className="font-medium text-indigo-600 hover:text-indigo-500"
+                              onClick={() => setOpenDashboard(false)}
+                            >
+                              Continue Shopping
+                              <span aria-hidden="true"> &rarr;</span>
+                            </button>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
     </>
   );
 };
--- silk-touch/src/pages/admin.tsx
+++ silk-touch/src/pages/admin.tsx
@@ -86,169 +86,187 @@ export default function Admin() {
     }));
   };
 
-  return (
-    <div>
-      <h1>Admin panel</h1>
-      <h2>Create an item</h2>
-      <form onSubmit={(e) => createItem(e)}>
-        <label htmlFor="title"> Title of item</label>
-        <input type="text" name="title"></input>
-        <label htmlFor="content"> Content of item</label>
-        <input type="text" name="content"></input>
-        <label htmlFor="price"> Price of item</label>
-        <input type="number" name="price"></input>
-        <label htmlFor="category"> Category of item</label>
-        <input type="text" name="category"></input>
-        <label htmlFor="subcategory"> Subcategory of item</label>
-        <input type="text" name="subcategory"></input>
-        <label htmlFor="published"> Is the item published?</label>
-        <input type="checkbox" name="published" value="published"></input>
-        <button type="submit">Create an item</button>
-      </form>
+  if (itemsQuery.isLoading && !itemsQuery.data) {
+    return <div>...</div>;
+  } else {
+    return (
+      <div>
+        <h1>Admin panel</h1>
+        <h2>Create an item</h2>
+        <form onSubmit={(e) => createItem(e)}>
+          <label htmlFor="title"> Title of item</label>
+          <input type="text" name="title"></input>
+          <label htmlFor="content"> Content of item</label>
+          <input type="text" name="content"></input>
+          <label htmlFor="price"> Price of item</label>
+          <input type="number" name="price"></input>
+          <label htmlFor="category"> Category of item</label>
+          <input type="text" name="category"></input>
+          <label htmlFor="subcategory"> Subcategory of item</label>
+          <input type="text" name="subcategory"></input>
+          <label htmlFor="published"> Is the item published?</label>
+          <input type="checkbox" name="published" value="published"></input>
+          <button type="submit">Create an item</button>
+        </form>
 
-      <div>Items list</div>
-      {itemsQuery.data.map((item) => {
-        return (
-          <div>
-            <form key={item.id} onSubmit={(e) => editItem(e, item.id)}>
-              <div>
+        <div>Items list</div>
+        {itemsQuery.data?.map((item) => {
+          return (
+            <div>
+              <form key={item.id} onSubmit={(e) => editItem(e, item.id)}>
                 <div>
-                  <div>{item.title}</div>
+                  <div>
+                    <div>{item.title}</div>
+                    {editItemId === item.id ? (
+                      <div>
+                        <label htmlFor="title"> Title of item</label>
+                        <input
+                          type="text"
+                          name="title"
+                          value={
+                            (editedData[item.id] &&
+                              editedData[item.id]?.title) !== undefined
+                              ? editedData[item.id]?.title
+                              : item.title
+                          }
+                          onChange={(e) =>
+                            handleInputChange(item.id, "title", e.target.value)
+                          }
+                        ></input>
+                      </div>
+                    ) : null}
+                  </div>
+                  <div>
+                    <div>{item.content}</div>
+                    {editItemId === item.id ? (
+                      <div>
+                        <label htmlFor="content"> Content of item</label>
+                        <input
+                          type="text"
+                          name="content"
+                          value={editedData[item.id]?.content || item.content}
+                          onChange={(e) =>
+                            handleInputChange(
+                              item.id,
+                              "content",
+                              e.target.value
+                            )
+                          }
+                        ></input>
+                      </div>
+                    ) : null}
+                  </div>
+                  <div>
+                    <div>{item.price}</div>
+                    {editItemId === item.id ? (
+                      <div>
+                        <label htmlFor="title"> Price of item</label>
+                        <input
+                          type="number"
+                          name="price"
+                          value={
+                            (editedData[item.id] &&
+                              editedData[item.id]?.price) !== undefined
+                              ? editedData[item.id]?.price
+                              : item.price
+                          }
+                          onChange={(e) =>
+                            handleInputChange(item.id, "price", e.target.value)
+                          }
+                        ></input>
+                      </div>
+                    ) : null}
+                  </div>
+                  <div>
+                    <div>{item.category}</div>
+                    {editItemId === item.id ? (
+                      <div>
+                        <label htmlFor="category"> Category of item</label>
+                        <input
+                          type="text"
+                          name="category"
+                          value={
+                            (editedData[item.id] &&
+                              editedData[item.id]?.category) !== undefined
+                              ? editedData[item.id]?.category
+                              : item.category
+                          }
+                          onChange={(e) =>
+                            handleInputChange(
+                              item.id,
+                              "category",
+                              e.target.value
+                            )
+                          }
+                        ></input>
+                      </div>
+                    ) : null}
+                  </div>
+                  <div>
+                    <div>{item.subcategory}</div>
+                    {editItemId === item.id ? (
+                      <div>
+                        <label htmlFor="subcategory">
+                          {" "}
+                          Subcategory of item
+                        </label>
+                        <input
+                          type="text"
+                          name="subcategory"
+                          value={
+                            (editedData[item.id] &&
+                              editedData[item.id]?.subcategory) !== undefined
+                              ? editedData[item.id]?.subcategory
+                              : item.subcategory
+                          }
+                          onChange={(e) =>
+                            handleInputChange(
+                              item.id,
+                              "subcategory",
+                              e.target.value
+                            )
+                          }
+                        ></input>
+                      </div>
+                    ) : null}
+                  </div>
+                  <div>
+                    <div>{item.published}</div>
+                    {editItemId === item.id ? (
+                      <div>
+                        <label htmlFor="published">
+                          {" "}
+                          Is the item published?
+                        </label>
+                        <input
+                          type="checkbox"
+                          name="published"
+                          value="published"
+                        ></input>
+                      </div>
+                    ) : null}
+                  </div>
+                  <button
+                    onClick={() => {
+                      if (editItemId === item.id) {
+                        setEditItemId(null); // Close the edit option
+                      } else {
+                        setEditItemId(item.id); // Open the edit option for this item
+                      }
+                    }}
+                  >
+                    {editItemId === item.id ? "Cancel Edit" : "Edit Product"}
+                  </button>
                   {editItemId === item.id ? (
-                    <div>
-                      <label htmlFor="title"> Title of item</label>
-                      <input
-                        type="text"
-                        name="title"
-                        value={
-                          (editedData[item.id] &&
-                            editedData[item.id]?.title) !== undefined
-                            ? editedData[item.id]?.title
-                            : item.title
-                        }
-                        onChange={(e) =>
-                          handleInputChange(item.id, "title", e.target.value)
-                        }
-                      ></input>
-                    </div>
+                    <button type="submit">Submit changes</button>
                   ) : null}
                 </div>
-                <div>
-                  <div>{item.content}</div>
-                  {editItemId === item.id ? (
-                    <div>
-                      <label htmlFor="content"> Content of item</label>
-                      <input
-                        type="text"
-                        name="content"
-                        value={editedData[item.id]?.content || item.content}
-                        onChange={(e) =>
-                          handleInputChange(item.id, "content", e.target.value)
-                        }
-                      ></input>
-                    </div>
-                  ) : null}
-                </div>
-                <div>
-                  <div>{item.price}</div>
-                  {editItemId === item.id ? (
-                    <div>
-                      <label htmlFor="title"> Price of item</label>
-                      <input
-                        type="number"
-                        name="price"
-                        value={
-                          (editedData[item.id] &&
-                            editedData[item.id]?.price) !== undefined
-                            ? editedData[item.id]?.price
-                            : item.price
-                        }
-                        onChange={(e) =>
-                          handleInputChange(item.id, "price", e.target.value)
-                        }
-                      ></input>
-                    </div>
-                  ) : null}
-                </div>
-                <div>
-                  <div>{item.category}</div>
-                  {editItemId === item.id ? (
-                    <div>
-                      <label htmlFor="category"> Category of item</label>
-                      <input
-                        type="text"
-                        name="category"
-                        value={
-                          (editedData[item.id] &&
-                            editedData[item.id]?.category) !== undefined
-                            ? editedData[item.id]?.category
-                            : item.category
-                        }
-                        onChange={(e) =>
-                          handleInputChange(item.id, "category", e.target.value)
-                        }
-                      ></input>
-                    </div>
-                  ) : null}
-                </div>
-                <div>
-                  <div>{item.subcategory}</div>
-                  {editItemId === item.id ? (
-                    <div>
-                      <label htmlFor="subcategory"> Subcategory of item</label>
-                      <input
-                        type="text"
-                        name="subcategory"
-                        value={
-                          (editedData[item.id] &&
-                            editedData[item.id]?.subcategory) !== undefined
-                            ? editedData[item.id]?.subcategory
-                            : item.subcategory
-                        }
-                        onChange={(e) =>
-                          handleInputChange(
-                            item.id,
-                            "subcategory",
-                            e.target.value
-                          )
-                        }
-                      ></input>
-                    </div>
-                  ) : null}
-                </div>
-                <div>
-                  <div>{item.published}</div>
-                  {editItemId === item.id ? (
-                    <div>
-                      <label htmlFor="published"> Is the item published?</label>
-                      <input
-                        type="checkbox"
-                        name="published"
-                        value="published"
-                      ></input>
-                    </div>
-                  ) : null}
-                </div>
-                <button
-                  onClick={() => {
-                    if (editItemId === item.id) {
-                      setEditItemId(null); // Close the edit option
-                    } else {
-                      setEditItemId(item.id); // Open the edit option for this item
-                    }
-                  }}
-                >
-                  {editItemId === item.id ? "Cancel Edit" : "Edit Product"}
-                </button>
-                {editItemId === item.id ? (
-                  <button type="submit">Submit changes</button>
-                ) : null}
-              </div>
-            </form>
-            <button onClick={() => deleteItem(item.id)}>Delete item</button>
-          </div>
-        );
-      })}
-    </div>
-  );
+              </form>
+              <button onClick={() => deleteItem(item.id)}>Delete item</button>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
