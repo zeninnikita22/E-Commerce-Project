ec68fc14260278f6726701151c697a77ce5011e0
Nikita <zeninnikita96@gmail.com>
2023-10-07 22:56:31 +0200

    work with search input and results CSS

--- silk-touch/src/pages/CartIcon.tsx
+++ silk-touch/src/pages/CartIcon.tsx
@@ -14,7 +14,6 @@ const CartIcon = () => {
   return (
     <>
       <div>
-        NUMBER
         {cartQuery.data?.reduce((totalQuantity, cartItem) => {
           return totalQuantity + cartItem.quantity;
         }, 0)}
--- silk-touch/src/pages/Search.tsx
+++ silk-touch/src/pages/Search.tsx
@@ -66,24 +66,24 @@ const Search = () => {
   function findItem(value) {
     console.log("finditem", value);
     setSearchInput(value);
-    // const searchedItems = itemsQuery.data.filter((item) => {
-    //   return (
-    //     value && item && item.title && item.title.toLowerCase().includes(value)
-    //   );
-    // });
-    // console.log(searchedItems);
   }
 
   return (
-    <>
-      {/* icon */}
-      <input
-        type="search"
-        placeholder="Type to search..."
-        value={searchInput}
-        onChange={(e) => findItem(e.target.value)}
-      ></input>
-      <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+    <div>
+      <div className="bg-pistachio flex justify-center alight-center">
+        <input
+          type="search"
+          placeholder="Type to search..."
+          value={searchInput}
+          onChange={(e) => findItem(e.target.value)}
+          className="my-4 mx-6 h-8 w-30 outline-none border-0 text-base rounded-full px-5 py-3 placeholder:text-gray placeholder:font-quicksand placeholder:text-sm"
+        ></input>
+      </div>
+      <div
+        className={`transition-transform duration-300 transform ${
+          searchInput === "" ? "-translate-y-full" : "translate-y-0"
+        } grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 bg-pistachio`}
+      >
         {itemsQuery.data?.length === 0
           ? "..."
           : searchInput === ""
@@ -98,44 +98,47 @@ const Search = () => {
               )
               .map((item) => {
                 return (
-                  <div key={item.id}>
-                    <div>
-                      <a href="#" className="group">
-                        <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-                          <img
-                            src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg"
-                            alt="Tall slender porcelain bottle with natural clay textured body and cork stopper."
-                            className="h-full w-full object-cover object-center group-hover:opacity-75"
-                          />
-                        </div>
-                        <h3 className="mt-4 text-sm text-gray-700">
-                          {item.title}
-                        </h3>
-                        <p className="mt-1 text-lg font-medium text-gray-900">
-                          ${item.price}
-                        </p>
-                      </a>
-                      <p className="mt-1 text-sm font-medium text-gray-300">
-                        {item.content}
+                  <div
+                    key={item.id}
+                    // className="transition-all duration-300 ease-in-out transform translate-x-10"
+                  >
+                    <a href="#" className="group">
+                      <div
+                      // className="overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7"
+                      >
+                        <img
+                          src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg"
+                          alt="Tall slender porcelain bottle with natural clay textured body and cork stopper."
+                          className="h-40 w-40"
+                        />
+                      </div>
+                      <h3 className="mt-4 text-sm text-gray-700">
+                        {item.title}
+                      </h3>
+                      <p className="mt-1 text-lg font-medium text-gray-900">
+                        ${item.price}
                       </p>
-                      <button
-                        onClick={() => addToCart(item)}
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                      >
-                        Add to cart
-                      </button>
-                      <button
-                        onClick={() => changeFavorites(item)}
-                        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-                      >
-                        Add to favorites
-                      </button>
-                    </div>
+                    </a>
+                    <p className="mt-1 text-sm font-medium text-gray-300">
+                      {item.content}
+                    </p>
+                    <button
+                      onClick={() => addToCart(item)}
+                      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    >
+                      Add to cart
+                    </button>
+                    <button
+                      onClick={() => changeFavorites(item)}
+                      className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+                    >
+                      Add to favorites
+                    </button>
                   </div>
                 );
               })}
       </div>
-    </>
+    </div>
   );
 };
 
