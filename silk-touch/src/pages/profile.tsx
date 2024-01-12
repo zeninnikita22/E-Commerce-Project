250493babe62c18281347a851c4fc365dd011ce0
Nikita <zeninnikita96@gmail.com>
2024-01-12 18:12:37 +0100

    fix profile settings

--- silk-touch/src/pages/profile.tsx
+++ silk-touch/src/pages/profile.tsx
@@ -1,6 +1,6 @@
 import { trpc } from "./utils/trpc";
 import { useQueryClient } from "@tanstack/react-query";
-import { useUser } from "@clerk/nextjs";
+import { SignInButton, useUser } from "@clerk/nextjs";
 import { UserProfile } from "@clerk/nextjs";
 
 export default function Profile() {
@@ -10,17 +10,27 @@ export default function Profile() {
   return (
     <div className="flex justify-center items-center">
       <div className="my-12">
-        <UserProfile
-          appearance={{
-            elements: {
-              card: "bg-off-white",
-              headerTitle: "font-quicksand",
-
-              formButtonPrimary:
-                "bg-pistachio text-black hover:bg-slate-300 text-sm normal-case font-raleway",
-            },
-          }}
-        ></UserProfile>
+        {!isSignedIn ? (
+          <div className="flex flex-col items-center gap-3">
+            <p>Please log in to view your profile</p>
+            <SignInButton>
+              <button className="bg-pistachio text-black font-raleway font-light py-2 px-8 rounded-full border border-transparent transition hover:border-black hover:border-opacity-100">
+                Log in
+              </button>
+            </SignInButton>
+          </div>
+        ) : (
+          <UserProfile
+            appearance={{
+              elements: {
+                card: "bg-off-white",
+                headerTitle: "font-quicksand",
+                formButtonPrimary:
+                  "bg-pistachio text-black hover:bg-slate-300 text-sm normal-case font-raleway",
+              },
+            }}
+          ></UserProfile>
+        )}
       </div>
     </div>
   );
